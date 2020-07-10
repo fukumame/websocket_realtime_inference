2fee8b85c249a7d75c6eddf1f7d2bae9786ab78c
fukumame <tanuma0115@gmail.com>
2020-07-10 15:43:03 +0900

    fix bugs

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ auth = HTTPDigestAuth()
 
 
 def _detect_face(img):
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
